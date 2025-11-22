a1c864816e0c49589e54797813d42b0e94606e60
Luis Carlos Gracia Puentes <123645497+luiscgracia@users.noreply.github.com>
2025-11-22 16:19:13 -0500

    ok

--- 98_pfm_traza_2025/Supply_Chain_Tracker/web/src/componentes/Homex.jsx
+++
@@ -1,9 +0,0 @@
-import {Outlet, Link} from 'react-router-dom'
-import { Header } from './Header'
-
-export function Homey() {
-  return <div>
-    <Header></Header>
-    <Outlet></Outlet>
-  </div>
-}

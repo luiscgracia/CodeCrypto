ee6918a65f25af0faee1473bed8229dfdd07ab56
Luis Carlos Gracia Puentes <123645497+luiscgracia@users.noreply.github.com>
2025-11-22 16:08:08 -0500

    ok

--- 98_pfm_traza_2025/Supply_Chain_Tracker/sc/test/SupplyChainTrackerTest.t.sol
+++ 98_pfm_traza_2025/Supply_Chain_Tracker/sc/test/SupplyChainTrackerTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "../lib/forge-std/src/Test.sol";
 import "../src/SupplyChainTracker.sol";
 
-contract SupplyChainTest is Test {
+contract SupplyChainTrackerTest is Test {
     SupplyChainTracker public supplyChain;
 
     // Roles
